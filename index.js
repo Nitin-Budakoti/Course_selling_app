b1e0b685137ff7ef7904ffd43c10c55a513f5eff
Nitin Budakoti <manuboda8885@gmail.com>
2025-03-09 08:54:25 +0530

    adding dot.env for hiding database connection

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const app = express();
-const mogoose = require("mongoose");
 const userRouter = require("./router/user");
 const courseRouter = require("./router/course");
 const adminRouter = require("./router/admin");
 const mongoose = require("mongoose");
-
+// using dotenv file to hide credentials login and passward 
+require("dotenv").config();
 //using Router method to handle endpoints
 app.use("/api/v1/user", userRouter);    //using Router method to handle endpoints
 app.use("/api/v1/admin",adminRouter);
 app.use("/api/v1/course", courseRouter);
 
 async function main(){
-await mongoose.connect("mongodb+srv://Nitin_budakoti:ysR4ALHkX6tDEdif@cluster0.ooku2.mongodb.net/course-era");
+let url = process.env.URI;
+await mongoose.connect(url);
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
